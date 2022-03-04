acc4e906a0126b743c1d013c515bff338dc783eb
FranSabt <hsabatino1@gmail.com>
2022-03-04 12:34:25 -0400

    falta arreglar el ejercicio para cuando hay más de dos argumentos

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray, element1, element2) {
+    let newArray = []
+    for(i = 0; i < myArray.length; i++ ){
+        if (myArray[i] === element1 || myArray[i] === element2){
+            continue;
+        }
+        else {
+            newArray.push(myArray[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
