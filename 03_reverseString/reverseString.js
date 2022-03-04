60b629504ecb4f1cb409d610bceae6ffd5ab5de7
FranSabt <hsabatino1@gmail.com>
2022-03-03 22:23:07 -0400

    recuerda que los string terminan en \0 para poder cerrarlos, sino, ese elemento dara UNDEFINED

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse = "";
+    for (i = string.length - 1; i >= 0; i--){ //se me olvido el carater de cierre del string
+    reverse += string[i];               //ahí estaba mi problema
+    }
+    return reverse;
 };
 
 // Do not edit below this line
