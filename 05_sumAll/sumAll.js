2e92a21f112e4062fcf72e06222f1e58709349f7
FranSabt <hsabatino1@gmail.com>
2022-03-05 23:53:55 -0400

    4 o 6 functions ready

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let result = 0;
+    if (a < 0 || b == isNaN){
+        return 'ERROR'
+    }
+    else {
+        if (a > b){
+            [a,b] = [b, a];
+        }
+        for (let i = a; i <= b; i++){
+            result = result + i; 
+        }
+        console.log(result)
+        console.log(a + "  " + b);
+        return result;
+    }
 };
 
 // Do not edit below this line
