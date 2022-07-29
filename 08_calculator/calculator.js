61c91cf62ff854b5e88951b8f81ea9e0a0c4d32a
FranSabt <hsabatino1@gmail.com>
2022-07-28 20:51:11 -0400

    calculator ready

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,40 @@
-const add = function() {
-	
+const add = (...arr) => {
+  let total = 0
+arr.map(num => {
+  total += num
+})
+return total
 };
 
-const subtract = function() {
-	
+const subtract = (...arr) => {
+  let total = 0
+arr.map(num => {
+  total === 0 ? total = num : total -= num
+})
+return total
 };
 
-const sum = function() {
-	
+const sum = (arr) => { //I have some problems with the spread operator
+                      //also their use was unnecessary
+  return arr.reduce((total, val) => total + val, 0)
 };
 
-const multiply = function() {
-
+const multiply = (arr ) => {
+  return arr.reduce((total, val) => total * val, 1)
+  // if your default value is 0, the result will always be 0
 };
 
-const power = function() {
-	
+const power = (a , b ) => {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = (n) => {
+  if(n === 0){
+    return 1
+  }
+  return n * factorial(n -1)
 };
+
 
 // Do not edit below this line
 module.exports = {
