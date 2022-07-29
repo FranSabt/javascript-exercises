5fbbf6f1a1f5d1e33cf8f0e6e77273510bc2bad0
FranSabt <hsabatino1@gmail.com>
2022-07-28 22:01:39 -0400

    regex ready

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = (word) => {
+  word = word.toUpperCase().replace(/[^a-z]/gi, '')
+  console.log(word);
+  for(let i = 0; i < word.length; i++){
+    if (word[i] !== word[(word.length - 1) - i]){
+      return false
+    }
+  }
+  return true
 };
 
 // Do not edit below this line
