963eb6edda0e30871e1c7b90ef183d0c50401e44
FranSabt <hsabatino1@gmail.com>
2022-07-29 09:21:45 -0400

    there's a way of making the function usin methods?

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = (num) => {
+  if(num < 1){
+    return 'OOPS'
+  }
+  let i = 1
+  let acc = 0
+  let oldN= 0
+  let count= 0
+  while(count < num){
+    oldN = acc
+    acc += i
+    i = oldN
+    count++
+  }
+  return acc
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
